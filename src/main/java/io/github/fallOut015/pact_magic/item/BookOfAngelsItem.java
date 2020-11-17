d9d0050a81eb8fd50a1aef78f628f9d5d1d239c2
fallOut015 <captainDoodles264@gmail.com>
2020-11-17 12:48:47 -0800

    modified books of entities

--- src/main/java/io/github/fallOut015/pact_magic/item/BookOfAngelsItem.java
+++ src/main/java/io/github/fallOut015/pact_magic/item/BookOfAngelsItem.java
@@ -1,13 +1,20 @@
 package io.github.fallOut015.pact_magic.item;
 
+import java.util.List;
+
 import io.github.fallOut015.pact_magic.client.gui.screen.inventory.BookOfAngelsScreen;
+import io.github.fallOut015.pact_magic.common.angels.Angel;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
 public class BookOfAngelsItem extends Item {
@@ -17,18 +24,32 @@ public class BookOfAngelsItem extends Item {
 	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack itemStack = playerIn.getHeldItem(handIn);
+		ItemStack stack = playerIn.getHeldItem(handIn);
 		 
 		if(worldIn.isRemote) {
-			return ActionResult.resultSuccess(itemStack);
+			return ActionResult.resultSuccess(stack);
 		} else {
-			Minecraft.getInstance().displayGuiScreen(new BookOfAngelsScreen(playerIn));
+			Minecraft.getInstance().displayGuiScreen(new BookOfAngelsScreen(playerIn, stack));
 			playerIn.addStat(Stats.ITEM_USED.get(this));
-			return ActionResult.resultConsume(itemStack);
+			return ActionResult.resultConsume(stack);
 		}
 	}
 	@Override
 	public boolean hasEffect(ItemStack stack) {
 		return true;
+	}
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+		
+		for(Angel angel : Angel.angels()) {
+			if(hasAngel(stack, angel)) {
+				tooltip.add(new TranslationTextComponent("gui." + angel.getID() + ".title").mergeStyle(TextFormatting.YELLOW, TextFormatting.ITALIC));
+			}
+		}
+	}
+	
+	public static boolean hasAngel(ItemStack stack, Angel angel) {
+		return stack.getOrCreateTag().contains(angel.getID()) && stack.getOrCreateTag().getBoolean(angel.getID()) == true;
 	}
 }
--- src/main/java/io/github/fallOut015/pact_magic/item/BookOfDemonsItem.java
+++ src/main/java/io/github/fallOut015/pact_magic/item/BookOfDemonsItem.java
@@ -1,13 +1,20 @@
 package io.github.fallOut015.pact_magic.item;
 
+import java.util.List;
+
 import io.github.fallOut015.pact_magic.client.gui.screen.inventory.BookOfDemonsScreen;
+import io.github.fallOut015.pact_magic.common.demons.Demon;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
 public class BookOfDemonsItem extends Item {
@@ -30,5 +37,19 @@ public class BookOfDemonsItem extends Item {
 	@Override
 	public boolean hasEffect(ItemStack stack) {
 		return true;
+	}
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+		
+		for(Demon demon : Demon.demons()) {
+			if(hasDemon(stack, demon)) {
+				tooltip.add(new TranslationTextComponent("gui." + demon.getID() + ".title").mergeStyle(TextFormatting.RED, TextFormatting.ITALIC));
+			}
+		}
+	}
+	
+	public static boolean hasDemon(ItemStack stack, Demon demon) {
+		return stack.getOrCreateTag().contains(demon.getID()) && stack.getOrCreateTag().getBoolean(demon.getID()) == true;
 	}
 }
