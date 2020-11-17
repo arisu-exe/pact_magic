6587c8362a79accc96156026d4ee3eacdaf03a23
fallOut015 <captainDoodles264@gmail.com>
2020-11-17 15:33:57 -0800

    changed page items
    added to search itemgroup and added right click

--- src/main/java/io/github/fallOut015/pact_magic/item/AngelPageItem.java
+++ src/main/java/io/github/fallOut015/pact_magic/item/AngelPageItem.java
@@ -1,14 +1,20 @@
 package io.github.fallOut015.pact_magic.item;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Nullable;
 
 import io.github.fallOut015.pact_magic.common.angels.Angel;
+import io.github.fallOut015.pact_magic.common.capabilities.CapabilitiesPactMagic;
+import io.github.fallOut015.pact_magic.common.capabilities.IPactMagic;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -29,7 +35,7 @@ public class AngelPageItem extends Item {
 	
 	@Override
 	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-		if(group == ItemGroup.MISC) {
+		if(group == ItemGroup.MISC || group == ItemGroup.SEARCH) {
 			for(Angel angel : Angel.angels()) {
 				ItemStack stack = new ItemStack(ItemsPactMagic.ANGEL_PAGE.get());
 				putAngel(stack, angel);
@@ -48,6 +54,28 @@ public class AngelPageItem extends Item {
 		@Nullable Angel angel = getAngel(stack);
 		if(angel != null) {
 			tooltip.add(new TranslationTextComponent("gui." + angel.getID() + ".title").mergeStyle(TextFormatting.YELLOW, TextFormatting.ITALIC));
+		}
+	}
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		
+		Optional<IPactMagic> pactMagic = playerIn.getCapability(CapabilitiesPactMagic.PACT_MAGIC).resolve();
+		
+		if(pactMagic.isPresent()) {
+			@Nullable Angel angel = getAngel(stack);
+			if(angel != null) {
+				if(pactMagic.get().getSlottedAngel() == angel) {
+					pactMagic.get().slotAngel(null);
+				} else {
+					pactMagic.get().slotAngel(angel);
+				}
+				return ActionResult.resultSuccess(stack);
+			} else {
+				return ActionResult.resultFail(stack);
+			}
+		} else {
+			return ActionResult.resultPass(stack);
 		}
 	}
 }
--- src/main/java/io/github/fallOut015/pact_magic/item/DemonPageItem.java
+++ src/main/java/io/github/fallOut015/pact_magic/item/DemonPageItem.java
@@ -1,14 +1,20 @@
 package io.github.fallOut015.pact_magic.item;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Nullable;
 
+import io.github.fallOut015.pact_magic.common.capabilities.CapabilitiesPactMagic;
+import io.github.fallOut015.pact_magic.common.capabilities.IPactMagic;
 import io.github.fallOut015.pact_magic.common.demons.Demon;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -29,7 +35,7 @@ public class DemonPageItem extends Item {
 	
 	@Override
 	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-		if(group == ItemGroup.MISC) {
+		if(group == ItemGroup.MISC || group == ItemGroup.SEARCH) {
 			for(Demon demon : Demon.demons()) {
 				ItemStack stack = new ItemStack(ItemsPactMagic.DEMON_PAGE.get());
 				putDemon(stack, demon);
@@ -48,6 +54,28 @@ public class DemonPageItem extends Item {
 		@Nullable Demon demon = getDemon(stack);
 		if(demon != null) {
 			tooltip.add(new TranslationTextComponent("gui." + demon.getID() + ".title").mergeStyle(TextFormatting.RED, TextFormatting.ITALIC));
+		}
+	}
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		
+		Optional<IPactMagic> pactMagic = playerIn.getCapability(CapabilitiesPactMagic.PACT_MAGIC).resolve();
+		
+		if(pactMagic.isPresent()) {
+			@Nullable Demon demon = getDemon(stack);
+			if(demon != null) {
+				if(pactMagic.get().getSlottedDemon() == demon) {
+					pactMagic.get().slotDemon(null);
+				} else {
+					pactMagic.get().slotDemon(demon);
+				}
+				return ActionResult.resultSuccess(stack);
+			} else {
+				return ActionResult.resultFail(stack);
+			}
+		} else {
+			return ActionResult.resultPass(stack);
 		}
 	}
 }
