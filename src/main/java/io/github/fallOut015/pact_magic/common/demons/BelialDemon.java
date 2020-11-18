ccb031880ed90e587f4be38bf0f014fbc9bac398
fallOut015 <captainDoodles264@gmail.com>
2020-11-18 14:27:27 -0800

    demons

--- src/main/java/io/github/fallOut015/pact_magic/common/demons/BelialDemon.java
+++ src/main/java/io/github/fallOut015/pact_magic/common/demons/BelialDemon.java
@@ -2,11 +2,12 @@ package io.github.fallOut015.pact_magic.common.demons;
 
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 
 public class BelialDemon extends Demon {
 	BelialDemon() {
-		super("belial", 1, Attributes.MOVEMENT_SPEED, Attributes.MAX_HEALTH, false, () -> Ingredient.fromItems());
+		super("belial", 1, Attributes.MOVEMENT_SPEED, Attributes.MAX_HEALTH, false, () -> Ingredient.fromItems(Items.CRAFTING_TABLE));
 	}
 
 	@Override
--- src/main/java/io/github/fallOut015/pact_magic/common/demons/MerihemDemon.java
+++ src/main/java/io/github/fallOut015/pact_magic/common/demons/MerihemDemon.java
@@ -2,11 +2,12 @@ package io.github.fallOut015.pact_magic.common.demons;
 
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 
 public class MerihemDemon extends Demon {
 	MerihemDemon() {
-		super("merihem", 1, Attributes.MOVEMENT_SPEED, Attributes.MAX_HEALTH, false, () -> Ingredient.fromItems());
+		super("merihem", 1, Attributes.MOVEMENT_SPEED, Attributes.MAX_HEALTH, false, () -> Ingredient.fromItems(Items.POISONOUS_POTATO));
 	}
 
 	@Override
