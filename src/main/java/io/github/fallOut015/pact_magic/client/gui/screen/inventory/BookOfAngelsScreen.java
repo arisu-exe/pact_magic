6bed754b1c0fb66247b558717d16a229e30e595e
fallOut015 <captainDoodles264@gmail.com>
2020-11-18 14:27:59 -0800

    temporary changes to the book screens

--- src/main/java/io/github/fallOut015/pact_magic/client/gui/screen/inventory/BookOfAngelsScreen.java
+++ src/main/java/io/github/fallOut015/pact_magic/client/gui/screen/inventory/BookOfAngelsScreen.java
@@ -5,10 +5,12 @@ import com.mojang.blaze3d.systems.RenderSystem;
 
 import io.github.fallOut015.pact_magic.client.gui.widget.button.AngelButton;
 import io.github.fallOut015.pact_magic.common.angels.Angels;
+import io.github.fallOut015.pact_magic.item.BookOfAngelsItem;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -33,27 +35,48 @@ public class BookOfAngelsScreen extends Screen {
 	protected Button buttonAngels;
 	
 	private final PlayerEntity player;
+	private final ItemStack stack;
 
-	public BookOfAngelsScreen(final PlayerEntity player) {
+	public BookOfAngelsScreen(final PlayerEntity player, final ItemStack stack) {
 		super(BOOK_OF_ANGELS);
+		
 		this.player = player;
+		this.stack = stack;
 	}
 	
 	@Override
 	protected void init() {
 		super.init();
 		
-		this.buttonSeraphim = this.addButton(new AngelButton(this.width / 18 + 2, 40, this.player, Angels.SERAPHIM, this));
-		this.buttonCherubim = this.addButton(new AngelButton(this.width / 18 + 130, 40, this.player, Angels.CHERUBIM, this));
-		this.buttonThrones = this.addButton(new AngelButton(this.width / 18 + 258, 40, this.player, Angels.THRONES, this));
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.SERAPHIM)) {
+			this.buttonSeraphim = this.addButton(new AngelButton(this.width / 18 + 2, 40, this.player, Angels.SERAPHIM, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.CHERUBIM)) {
+			this.buttonCherubim = this.addButton(new AngelButton(this.width / 18 + 130, 40, this.player, Angels.CHERUBIM, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.THRONES)) {
+			this.buttonThrones = this.addButton(new AngelButton(this.width / 18 + 258, 40, this.player, Angels.THRONES, this));
+		}
 
-		this.buttonDominions = this.addButton(new AngelButton(this.width / 18 + 2, 106, this.player, Angels.DOMINIONS, this));
-		this.buttonVirtues = this.addButton(new AngelButton(this.width / 18 + 130, 106, this.player, Angels.VIRTUES, this));
-		this.buttonPowers = this.addButton(new AngelButton(this.width / 18 + 258, 106, this.player, Angels.POWERS, this));
-
-		this.buttonPrincipalities = this.addButton(new AngelButton(this.width / 18 + 2, 172, this.player, Angels.PRINCIPALITIES, this));
-		this.buttonArchangels = this.addButton(new AngelButton(this.width / 18 + 130, 172, this.player, Angels.ARCHANGELS, this));
-		this.buttonAngels = this.addButton(new AngelButton(this.width / 18 + 258, 172, this.player, Angels.ANGELS, this));
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.DOMINIONS)) {
+			this.buttonDominions = this.addButton(new AngelButton(this.width / 18 + 2, 106, this.player, Angels.DOMINIONS, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.VIRTUES)) {
+			this.buttonVirtues = this.addButton(new AngelButton(this.width / 18 + 130, 106, this.player, Angels.VIRTUES, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.POWERS)) {
+			this.buttonPowers = this.addButton(new AngelButton(this.width / 18 + 258, 106, this.player, Angels.POWERS, this));
+		}
+		
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.PRINCIPALITIES)) {
+			this.buttonPrincipalities = this.addButton(new AngelButton(this.width / 18 + 2, 172, this.player, Angels.PRINCIPALITIES, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.ARCHANGELS)) {
+			this.buttonArchangels = this.addButton(new AngelButton(this.width / 18 + 130, 172, this.player, Angels.ARCHANGELS, this));
+		}
+		if(BookOfAngelsItem.hasAngel(this.stack, Angels.ANGELS)) {
+			this.buttonAngels = this.addButton(new AngelButton(this.width / 18 + 258, 172, this.player, Angels.ANGELS, this));
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
--- src/main/java/io/github/fallOut015/pact_magic/client/gui/screen/inventory/BookOfDemonsScreen.java
+++ src/main/java/io/github/fallOut015/pact_magic/client/gui/screen/inventory/BookOfDemonsScreen.java
@@ -5,10 +5,12 @@ import com.mojang.blaze3d.systems.RenderSystem;
 
 import io.github.fallOut015.pact_magic.client.gui.widget.button.DemonButton;
 import io.github.fallOut015.pact_magic.common.demons.Demons;
+import io.github.fallOut015.pact_magic.item.BookOfDemonsItem;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -33,27 +35,48 @@ public class BookOfDemonsScreen extends Screen {
 	protected Button buttonMerihem;
 	
 	private final PlayerEntity player;
+	private final ItemStack stack;
 
-	public BookOfDemonsScreen(final PlayerEntity player) {
+	public BookOfDemonsScreen(final PlayerEntity player, final ItemStack stack) {
 		super(BOOK_OF_DEMONS);
+		
 		this.player = player;
+		this.stack = stack;
 	}
 	
 	@Override
 	protected void init() {
 		super.init();
 		
-		this.buttonSatan = this.addButton(new DemonButton(this.width / 18 + 2, 40, this.player, Demons.SATAN, this));
-		this.buttonBeelzebub = this.addButton(new DemonButton(this.width / 18 + 258, 40, this.player, Demons.BEELZEBUB, this));
-		this.buttonAstaroth = this.addButton(new DemonButton(this.width / 18 + 130, 40, this.player, Demons.ASTAROTH, this));
-
-		this.buttonAbaddon = this.addButton(new DemonButton(this.width / 18 + 2, 106, this.player, Demons.ABADDON, this));
-		this.buttonMammon = this.addButton(new DemonButton(this.width / 18 + 130, 106, this.player, Demons.MAMMON, this));
-		this.buttonAsmodeus = this.addButton(new DemonButton(this.width / 18 + 258, 106, this.player, Demons.ASMODEUS, this));
-
-		this.buttonPytho = this.addButton(new DemonButton(this.width / 18 + 2, 172, this.player, Demons.PYTHO, this));
-		this.buttonBelial = this.addButton(new DemonButton(this.width / 18 + 130, 172, this.player, Demons.BELIAL, this));
-		this.buttonMerihem = this.addButton(new DemonButton(this.width / 18 + 258, 172, this.player, Demons.MERIHEM, this));
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.SATAN)) {
+			this.buttonSatan = this.addButton(new DemonButton(this.width / 18 + 2, 40, this.player, Demons.SATAN, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.BEELZEBUB)) {
+			this.buttonBeelzebub = this.addButton(new DemonButton(this.width / 18 + 258, 40, this.player, Demons.BEELZEBUB, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.ASTAROTH)) {
+			this.buttonAstaroth = this.addButton(new DemonButton(this.width / 18 + 130, 40, this.player, Demons.ASTAROTH, this));
+		}
+		
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.ABADDON)) {
+			this.buttonAbaddon = this.addButton(new DemonButton(this.width / 18 + 2, 106, this.player, Demons.ABADDON, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.MAMMON)) {
+			this.buttonMammon = this.addButton(new DemonButton(this.width / 18 + 130, 106, this.player, Demons.MAMMON, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.ASMODEUS)) {
+			this.buttonAsmodeus = this.addButton(new DemonButton(this.width / 18 + 258, 106, this.player, Demons.ASMODEUS, this));
+		}
+		
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.PYTHO)) {
+			this.buttonPytho = this.addButton(new DemonButton(this.width / 18 + 2, 172, this.player, Demons.PYTHO, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.BELIAL)) {
+			this.buttonBelial = this.addButton(new DemonButton(this.width / 18 + 130, 172, this.player, Demons.BELIAL, this));
+		}
+		if(BookOfDemonsItem.hasDemon(this.stack, Demons.MERIHEM)) {
+			this.buttonMerihem = this.addButton(new DemonButton(this.width / 18 + 258, 172, this.player, Demons.MERIHEM, this));
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
